2cac8bb8e5bb74e1af7e3dca8d136da5c4e993cc
EliasGalindo0 <eliasgalindo@gmail.com>
2022-06-13 17:37:19 -0300

    fix: css style mobile - tech skills

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,10 +10,10 @@ export default function About() {
         {showAbout ? <h1>About ⏬</h1> : <h1>About ⏫</h1>}
       </div>
       { !showAbout ? (
-        <p>
+        <p className="about">
           I'm love technology. Decided to study web development to transit from
           lawyer carrer to web developer. For that, I started to study at Trybe
-          school, where I already lerned front-end stack with HTML, CSS,
+          school, where I already learned front-end stack with HTML, CSS,
           Javascript, React, Redux and Context API. I'm 35 and live in
           Presidente Prudente/SP.
         </p>
