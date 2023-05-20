0ffb7289a102dfc025d643258ee975b72ea21be7
Elias Galindo <eliasgalindo@gmail.com>
2023-05-20 10:44:29 -0300

    update branch

--- src/data/front.js
+++ src/data/front.js
@@ -3,6 +3,9 @@ import reactTest from './projects/front/rtl-test.gif';
 import recipesApp from './projects/front/recipeApp.gif';
 import planets from './projects/front/planets.gif';
 import starwars from './projects/front/starwars.gif';
+import trivia from './projects/front/trivia.gif';
+import trybetunes from './projects/front/trybetunes.gif';
+import trybewallet from './projects/front/trybewallet.gif';
 
 const front = [
   { name: 'Front-end Online Store', url: 'https://github.com/EliasGalindo0/project-frontend-online-store', image: frontStore },
@@ -10,6 +13,9 @@ const front = [
   { name: 'Recipes App', url: 'https://github.com/EliasGalindo0/project-front-recipes-app', image: recipesApp },
   { name: 'Solar System', url: 'https://github.com/EliasGalindo0/project-front-solar-system', image: planets },
   { name: 'Star Wars', url: 'https://github.com/EliasGalindo0/project-front-starwars-planets-search', image: starwars },
+  { name: 'Trivia', url: 'https://github.com/EliasGalindo0/project-front-trivia', image: trivia },
+  { name: 'TrybeTunes', url: 'https://github.com/EliasGalindo0/project-front-trybetunes', image: trybetunes },
+  { name: 'TrybeWallet', url: 'https://github.com/EliasGalindo0/project-front-trybewallet', image: trybewallet },
 ];
 
 export default front;
