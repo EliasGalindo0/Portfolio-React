7869a74ba9b50abb033bf5a1585b85baa5e709f1
Elias Galindo <eliasgalindo@gmail.com>
2023-06-05 15:22:51 -0300

    Feat: Change about section and update styles

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,9 +15,38 @@ export default function About() {
       </div>
       { !showAbout ? (
         <p className="about">
-          I love technology. I`m a full stack web developer and currently
-          I`m studying Software Engineering.
-          I`m 35 years old and I live in Presidente Prudente, São Paulo, Brazil.
+          As a software engineering student,
+          I have gained valuable experience in the field through my work as a
+          Full Stack Software Developer at
+          NeoGest - Agência Digital and Evartel - Controle de Ponto.
+          During my time at NeoGest,
+          I was responsible for developing modules in PHP,
+          creating databases, validating user information,
+          creating client administrative environments,
+          creating payment integrations and modules, simulating freight,
+          validating and mapping data, consuming REST APIs,
+          and styling front-end with Bootstrap.
+          At Evartel, I continued to develop
+          my full stack web development skills in PHP and JavaScript,
+          while also maintaining the system and resolving bugs,
+          and implementing new technologies and functionalities.
+          <br />
+          <br />
+          In addition to my work experience,
+          I also have experience as a Professor at Proz, where
+          I taught the subject of digital world security.
+          My responsibilities included developing research activities
+          and lectures, as well as teaching topics such as HTTPS protocol,
+          cryptography,
+          social networks, data security, network security, and cyber attacks.
+          <br />
+          <br />
+          Through my experiences,
+          I have developed a strong understanding of software development,
+          security, and the importance of staying up-to-date with the latest
+          technologies and best practices.
+          I am committed to continuing my education and growth in the field,
+          and am excited to see where my career in software engineering takes me.
         </p>
       ) : null}
     </div>
